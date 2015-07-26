9546ae517483d968895ff3f9d770da3539f690da
Volney Tejeda <volneytejeda10@gmail.com>
2015-07-26 13:49:30 -0400

    add test for flash, errors, and gravatar

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,12 +14,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information" do
     get signup_path
-    assert_difference "User.count", 1 do
-      post_via_redirect, user: { name: "Example user",
-                                  email: "user@example.org",
-                                  password: "password",
-                                  password_confirmation: "password"}
-      end
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
     assert_template 'users/show'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_not flash.empty?
   end
 end
