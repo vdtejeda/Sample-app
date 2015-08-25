23a12cc3832aafdf9a92e8c105738f72b1b70265
Volney Tejeda <volneytejeda10@gmail.com>
2015-08-25 15:49:45 -0400

    Add Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -62,7 +63,7 @@ class User < ActiveRecord::Base
    def password_reset_expired?
      reset_sent_at < 2.hours.ago
    end
-   
+
   private
 
   def downcase_email
