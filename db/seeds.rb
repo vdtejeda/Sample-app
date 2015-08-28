9adfa15840f44e567898a6ce66b8c03ca684935f
Volney Tejeda <volneytejeda10@gmail.com>
2015-08-27 23:40:26 -0400

    fix seed file 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name:"Example User",
-             email: "example@railstutorial.org",
+             email: "differentexample@railstutorial.org",
              password: "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -15,7 +15,7 @@ User.create!(name:"Example User",
 
 99.times do |n|
   name  = Faker::Name.name
-  n = 0 
+  n = 0
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
