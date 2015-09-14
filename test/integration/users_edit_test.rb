78c99147e93b0e7c8b84f8f4dec9f44e1bbf2e4a
Volney Tejeda <volneytejeda10@gmail.com>
2015-09-13 22:09:03 -0400

    Add test for friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,6 +18,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
     name  = "Foo Bar"
@@ -28,9 +29,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     password_confirmation: "" }
     assert_not flash.empty?
     assert_redirected_to @user
+    assert_nil session[:forwarding_url]
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
-  
+
 end
