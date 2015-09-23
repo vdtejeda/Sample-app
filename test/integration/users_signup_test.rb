1cf5475812f8bd3595d234e073ae537edcda0034
Volney Tejeda <volneytejeda10@gmail.com>
2015-09-23 16:12:47 -0400

    Add test for flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -55,6 +55,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
      assert user.reload.activated?
      follow_redirect!
      assert_template 'users/show'
+     assert_not_nil flash
      assert is_logged_in?
    end
 end
