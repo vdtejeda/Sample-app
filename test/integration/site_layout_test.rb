283cfccf0d5bd072db1d4ec341c879728959f2fb
Volney Tejeda <volneytejeda10@gmail.com>
2015-09-30 15:06:02 -0400

    Add test for signup page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
     assert_select "a[href=?]", login_path
     user = users(:michael)
     log_in_as(user)
